f1cebb7547fdab19746a218d6bdf952b2e0b0912
Pitts <pittss@wit.edu>
2019-02-04 17:00:00 -0500

    Fixed messy code, bad naming, added support for time ranges rather than half hour blocks, using async

--- makeSchedule.py
+++ makeSchedule.py
@@ -4,127 +4,137 @@ import math
 class MainFrame(tk.Frame):
     def __init__(self,root=tk.Tk(),*args,**kwargs):
         tk.Frame.__init__(self,root,*args,**kwargs)
-        self.fileLoadedString = tk.StringVar()
-        self.fileLoadedString.set("No file loaded")
-        self.fileLoadedLabel = tk.Label(self,textvariable=self.fileLoadedString)
+        self.file_loaded_string = tk.StringVar()
+        self.file_loaded_string.set("No file loaded")
+        self.file_loaded_label = tk.Label(self,textvariable=self.file_loaded_string)
         self.master = root
-        self.employeesDict = None #Dict of employees that map to a list of tuples of (weekday,timenum) eg ('M',7.5)
-        self.loadFileButton = tk.Button(self,text="Load People File",command=self.loadPeopleFile)
-        self.newFileButton = tk.Button(self,text="Create new People file", command=self.newPeopleFile)
-        self.deleteEmployeeButton = tk.Button(self,text="Remove employee",command=self.removeEmployee)  #delete employee record
-        self.employeesFileName = None
-        self.loadFileButton.pack()
-        self.newFileButton.pack()
-        self.deleteEmployeeButton.pack()
-        self.newEmployeeButton = tk.Button(self,text="New employee",command=self.createNewEmployee)
-        self.employeeButtonFrame = tk.Frame(self)
-        self.editEmployeeButton= tk.Button(self,text="Edit employee hours",command=self.employeeButtonFrame.pack)
+        self.employees_dict = None #Dict of employees that map to a list of tuples of (weekday,timenum) eg ('M',7.5)
+        self.load_file_button = tk.Button(self,text="Load People File",command=self.loadPeopleFile)
+        self.new_file_button = tk.Button(self,text="Create new People file", command=self.new_people_file)
+        self.delete_employee_button = tk.Button(self,text="Remove employee",command=self.removeEmployee)  #delete employee record
+        self.employees_filename = None
+        self.load_file_button.pack()
+        self.new_file_button.pack()
+        self.delete_employee_button.pack()
+        self.new_employee_button = tk.Button(self,text="New employee",command=self.create_new_employee)
+        self.employee_button_frame = tk.Frame(self)
+        self.edit_employee_button= tk.Button(self,text="Edit employee hours",command=self.employee_button_frame.pack)
         self.master.geometry('{}x{}'.format(800,800))
-        self.saveButton = tk.Button(self,text="Save file",command=self.save)
-        self.saveAndQuitButton = tk.Button(self,text="Save and exit",command=self.saveAndQuit)
+        self.save_button = tk.Button(self,text="Save file",command=self.save)
+        self.save_and_quit_button = tk.Button(self,text="Save and exit",command=self.save_and_quit)
         self.pack()
     def loadPeopleFile(self): #return (or set variable) to dict if success, else keep as none and throw an error
         filename = filedialog.askopenfilename(parent=self,title="Please open the txt file of the employees.",filetypes=(("Text files","*.txt"),("All files","*.*")))
         with open(filename) as f:
-            self.employeesDict = eval(f.read())
-        self.employeesFileName = filename
-        self.loadFileButton.pack_forget()
-        self.newFileButton.pack_forget()
-        buttons = [tk.Button(self.employeeButtonFrame,text=name,command=lambda name=name:self.editEmployeeHours(name)) for name in self.employeesDict.keys()]
+            self.employees_dict = eval(f.read())
+        self.employees_filename = filename
+        self.load_file_button.pack_forget()
+        self.new_file_button.pack_forget()
+        buttons = [tk.Button(self.employee_button_frame,text=name,command=lambda name=name:self.edit_employee_hours(name)) for name in self.employees_dict.keys()]
         for button in buttons: button.pack()
-        self.newEmployeeButton.pack()
-        self.editEmployeeButton.pack()
-        self.saveButton.pack()
-    def newPeopleFile(self):
+        self.new_employee_button.pack()
+        self.edit_employee_button.pack()
+        self.save_button.pack()
+    def new_people_file(self):
         filename = filedialog.asksaveasfilename(title="Where should it be saved?",filetypes=(("Text files","*.txt"),("All files","*.*")))
-        self.employeesDict = dict()
-        self.employeesFileName = filename
-        self.loadFileButton.pack_forget()
-        self.newFileButton.pack_forget()
-        self.newEmployeeButton.pack()
-        self.editEmployeeButton.pack()
-        self.saveButton.pack()
-    def createNewEmployee(self):
-        if(self.employeesDict == None):
+        self.employees_dict = dict()
+        self.employees_filename = filename
+        self.load_file_button.pack_forget()
+        self.new_file_button.pack_forget()
+        self.new_employee_button.pack()
+        self.edit_employee_button.pack()
+        self.save_button.pack()
+    def create_new_employee(self):
+        if(self.employees_dict == None):
             tk.messagebox.showwarning("Warning","No file is open")
             return
         name = simpledialog.askstring("Employee name","Please enter the name of the employee")
-        self.employeesDict[name] = []
-        self.editEmployeeHours(name)
-        tk.Button(self.employeeButtonFrame,text=name,command=lambda:self.editEmployeeHours(name)).pack()
+        self.employees_dict[name] = []
+        self.edit_employee_hours(name)
+        tk.Button(self.employee_button_frame,text=name,command=lambda:self.edit_employee_hours(name)).pack()
     def removeEmployee(self):#TODO: depricated. remove dictionary enrty
-        employeeName = simpledialog.askstring("Employee name","Please enter the name of the employee")
+        employee_name = simpledialog.askstring("Employee name","Please enter the name of the employee")
         try:
-            for name in employeesDict:
-                if name==employeeName:
-                    del self.employeesDict[employeeName]
+            for name in employees_dict:
+                if name==employee_name:
+                    del self.employees_dict[employee_name]
             print("Employee removed")
         except:
             print("employee not found")
-    def editEmployeeHours(self,employeeName):
-        print(str(employeeName)+" "+str(self.employeesDict[employeeName]))
-        self.employeeButtonFrame.pack_forget()
-        employeeGrid = InputBoxes(self,employeeName=employeeName,employeeList=self.employeesDict[employeeName])
-        employeeGrid.pack()
-        closeButton = tk.Button(self,text="Save employee hours",command=lambda:self.saveEmployeeHours(employeeName,employeeGrid,closeButton)) #Can I pass the button here?
-        closeButton.pack()
-        self.editEmployeeButton.pack_forget()
-        self.newEmployeeButton.pack_forget()
-    def saveEmployeeHours(self,employeeName,InputBoxes,closeButton):
-        self.employeesDict[employeeName] = InputBoxes.asList()
-        InputBoxes.pack_forget()
-        closeButton.pack_forget()
-        self.editEmployeeButton.pack()
-        self.newEmployeeButton.pack()
+    def edit_employee_hours(self,employee_name):
+        self.employee_button_frame.pack_forget()
+        employee_input_boxes = InputBoxes(self)
+        employee_input_boxes.pack()
+        close_button = tk.Button(self,text="Save employee hours",command=lambda:self.save_employee_hours(employee_name,employee_input_boxes,close_button)) #Can I pass the button here?
+        close_button.pack()
+        self.edit_employee_button.pack_forget()
+        self.new_employee_button.pack_forget()
+    def save_employee_hours(self,employee_name,employee_input_boxes,close_button):
+        self.employees_dict[employee_name] = employee_input_boxes.as_range_lists_dict()
+        employee_input_boxes.pack_forget()
+        close_button.pack_forget()
+        self.edit_employee_button.pack()
+        self.new_employee_button.pack()
     def save(self):
-        with open(self.employeesFileName,'w') as f:
-            f.write(str(self.employeesDict))
-    def saveAndQuit(self):
+        with open(self.employees_filename,'w') as f:
+            f.write(str(self.employees_dict))
+    def save_and_quit(self):
         self.save()
         self.master.quit()
 class InputBoxes(tk.Frame):   #input start/end times for shift
-    def __init__(self,root,employeeName=None,employeeList=[],*args,**kwargs):
+    def __init__(self,root,*args,**kwargs):
         tk.Frame.__init__(self,root,*args,**kwargs)
         self.master = root
-        self.daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
-        self.daysOfWeekShort = ['M','T','W','R','F']
-        self.entryBoxes= {day:tk.Entry(self)for day in self.daysOfWeek}
-        for day in self.daysOfWeek:
+        self.days_of_week = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+        self.days_of_week_short = ['M','T','W','R','F']
+        self.entry_boxes= {day:tk.Entry(self)for day in self.days_of_week}
+        for day in self.days_of_week:
             tk.Label(self,text=day+": ").pack()
-            self.entryBoxes[day].pack()
-    def timesList(self,timesString):
-        startTime,endTime=timesString.split('-')#splits start and end times
-        startHourTemp,startMin=startTime.split(':')#splits start time into hours and minutes
-        startSuffix=startMin[-2:]
-        startMin=float(startMin[:-2])
-        endHourTemp,endMin=endTime.split(':')
-        endSuffix=endMin[-2:]
-        endMin=float(endMin[:-2])
-        startHour=float(startHourTemp)#converts start hours into int
-        endHour=float(endHourTemp)
-        shiftStart=self.timeConversion(startHour,startMin,startSuffix)
-        shiftEnd=self.timeConversion(endHour,endMin,endSuffix)
-        shiftDuration=shiftEnd-shiftStart
-        shiftDec=startHour+(startMin/60)#time shift begins in decimal format, i.e.: 8:30am=8.5
-        shiftList=[shiftDec]#list of start times for each half-hour segment of a shift
-        while (shiftDuration>0):
-            shiftDuration-=30
-            shiftDec+=0.5
-            shiftList.append(shiftDec)
-        return shiftList
-    def timeConversion(self,hour,minute,suffix):#simplifies process of calculating shift times
-        if (suffix=="PM"):
-            hour+=12
-        timeStamp=(hour*60)+minute
-        return timeStamp
-    def asList(self):
-        full_list = []
-        for day_of_week_index,day_of_week in enumerate(self.daysOfWeek):
-            short_day_of_week = self.daysOfWeekShort[day_of_week_index]
-            day_of_week_blocks = self.timesList(self.entryBoxes[day_of_week].get())
-            for block in day_of_week_blocks:
-                full_list.append((short_day_of_week,block))
-        return full_list
+            self.entry_boxes[day].pack()
+    def time_floats_from_str(self,times:str):
+        start_time,end_time=times.split('-')#splits start and end times
+        start_time_float = self.time_str_to_float(start_time)
+        end_time_float = self.time_str_to_float(end_time)
+        return start_time_float,end_time_float
+        #times_list = [
+        #return [my_float for my_float in range(start_time_float,end_time_float-0.25,0.5)]
+        # startHourTemp,startMin=startTime.split(':')#splits start time into hours and minutes
+        # startSuffix=startMin[-2:]
+        # startMin=float(startMin[:-2])
+        # endHourTemp,endMin=endTime.split(':')
+        # endSuffix=endMin[-2:]
+        # endMin=float(endMin[:-2])
+        # startHour=float(startHourTemp)#converts start hours into int
+        # endHour=float(endHourTemp)
+        # shiftStart=self.timeConversion(startHour,startMin,startSuffix)
+        # shiftEnd=self.timeConversion(endHour,endMin,endSuffix)
+        # shiftDuration=shiftEnd-shiftStart
+        # shiftDec=startHour+(startMin/60)#time shift begins in decimal format, i.e.: 8:30am=8.5
+        # shiftList=[shiftDec]#list of start times for each half-hour segment of a shift
+        # while (shiftDuration>0):
+        #     shiftDuration-=30
+        #     shiftDec+=0.5
+        #     shiftList.append(shiftDec)
+        # return shiftList
+    def times_list_from_day(self,times:str):
+        return [self.time_floats_from_str(timerange) for timerange in times.split(',')]
+    def time_str_to_float(self,time:str):
+        pm_modifier = 12 if "pm" in time.lower() else 0
+        hour,min_with_suffix = time.split(':')
+        hour = int(hour)+pm_modifier
+        minute = int(min_with_suffix[:-2])
+        return hour+(minute/60)
+        
+    def as_range_lists_dict(self):
+        return {day_of_week:self.times_list_from_day(self.entry_boxes[day_of_week].get()) for day_of_week in self.days_of_week}
+    # def asList(self):
+    #     full_list = []
+    #     for day_of_week_index,day_of_week in enumerate(self.daysOfWeek):
+    #         short_day_of_week = self.daysOfWeekShort[day_of_week_index]
+    #         day_of_week_blocks = self.times_list_from_day(self.entryBoxes[day_of_week].get())
+    #         for block in day_of_week_blocks:
+    #             full_list.append((short_day_of_week,block))
+    #     return full_list
     
     
 
--- showSchedule.py
+++ showSchedule.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 import datetime
 import time
-import _thread
 from tkinter import filedialog, simpledialog
 from tkinter import messagebox
-#import conf.py
+import asyncio
 
 
 bfs=10 #base font size
@@ -12,142 +11,101 @@ bfs=10 #base font size
 class Application(tk.Frame):
     def __init__(self,root=None,*args,**kwargs):
         tk.Frame.__init__(self,root,*args,**kwargs)
-        self.employeeDict = loadPeopleFile(self)
-        self.locationDesc = simpledialog.askstring("Location","Please enter a description of this schedule, such as \"Library\"")
-        self.weekday = getWeekdayAsChar()
-        self.currentTime = getTimeAsHalfInt()
-        self.hereNowFrame = PeopleFrame(root=self,title="Here now:")
-        self.leavingSoonFrame = PeopleFrame(root=self,title="Leaving soon:")
-        self.arrivingSoonFrame = PeopleFrame(root=self,title="Arriving soon:")
-        self.timeFrame = TimeFrame(root=self)
-        self.descriptionFrame = DescriptionFrame(root=self)
-        self.hereNowFrame.grid(row=2,column=0,rowspan=2,padx=50,pady=50,sticky='N')
-        self.leavingSoonFrame.grid(row=2,column=1,padx=50,pady=50)
-        self.arrivingSoonFrame.grid(row=3,column=1,padx=50,pady=50)
-        self.timeFrame.grid(row=1,column=0,columnspan=3,padx=15,pady=15,sticky="N")
-        self.timeFrame.config(borderwidth=10)
-        self.descriptionFrame.grid(row=0,column=0,columnspan=3, padx=10, pady=10, sticky="N")
-        
-        
-        
-        #self.infoButtonCommand = showAboutBox
-        #self.infoButtonFrame = tk.Frame(self)
-        #self.infoButton = tk.Button(self.infoButtonFrame,text="?", command=lambda:print("8313"))
-        #self.infoButtonFrame.grid(row=3,column=1)
-        #self.infoButton.pack()
-        #print("1")
-        _thread.start_new_thread(self.alwaysUpdate,tuple())
-        self.timeChanged()
+        self.employee_dict = load_people_file(self)
+        self.location_description = simpledialog.askstring("Location","Please enter a description of this schedule, such as \"Library\"")
+        self.here_now_frame = PeopleFrame(root=self,title="Here now:")
+        self.leaving_soon_frame = PeopleFrame(root=self,title="Leaving soon:")
+        self.arriving_soon_frame = PeopleFrame(root=self,title="Arriving soon:")
+        self.time_frame = time_frame(root=self)
+        self.description_frame = DescriptionFrame(root=self)
+        self.here_now_frame.grid(row=2,column=0,rowspan=2,padx=50,pady=50,sticky='N')
+        self.leaving_soon_frame.grid(row=2,column=1,padx=50,pady=50)
+        self.arriving_soon_frame.grid(row=3,column=1,padx=50,pady=50)
+        self.time_frame.grid(row=1,column=0,columnspan=3,padx=15,pady=15,sticky="N")
+        self.time_frame.config(borderwidth=10)
+        self.description_frame.grid(row=0,column=0,columnspan=3, padx=10, pady=10, sticky="N")
         self.master.geometry('{}x{}'.format(800,800))
         self.pack()
         print("Steven Pitts\nMade for the TechSpot\nMaku")
-        #print("2")
-    #def showInfoBox(self):
-    #    print("ihewofa")
-    #    #messagebox.showinfo("About","Steven Pitts\nMade for the TechSpot\nMaku")
         
-    def alwaysUpdate(self):
+    async def always_update(self):
         while True:
-            newTime = getTimeAsHalfInt()
-            timeDifferent = not (self.currentTime == newTime)
-            if timeDifferent:
-                self.weekday = getWeekdayAsChar()
-                self.currentTime = getTimeAsHalfInt()
-                self.timeChanged()
-            self.timeFrame.updateTimeFrame()
-            time.sleep(0.1)
-    def timeChanged(self):
-        print("Time has changed")
-        timeTuple = (self.weekday,self.currentTime)
-        nextTimeTuple = (self.weekday,self.currentTime + 0.5)
-        hereNowList = []
-        leavingSoonList = []
-        arrivingSoonList = []
+            try:
+                self.update_idletasks()
+                self.update()
+                self.time_changed()
+                self.time_frame.update_time_frame()
+            except tk.TclError:
+                return
+    def time_changed(self):
+        current_time = get_current_time_as_float()
+        here_now_list = []
+        leaving_soon_list = []
+        arriving_soon_list = []
         #If they're leaving soon, don't include in "here now".
-        for person in self.employeeDict.keys():
-            personTimes = self.employeeDict[person]
-            personHereNow = timeTuple in personTimes
-            personHereNext = nextTimeTuple in personTimes
-            if personHereNow and personHereNext:
-                hereNowList.append(person)
-            elif personHereNow and not personHereNext:
-                leavingSoonList.append(person)
-            elif personHereNext and not personHereNow:
-                arrivingSoonList.append(person)
-        self.hereNowFrame.setPeopleList(hereNowList)
-        self.leavingSoonFrame.setPeopleList(leavingSoonList)
-        self.arrivingSoonFrame.setPeopleList(arrivingSoonList)
+        for person in self.employee_dict.keys():
+            person_times = self.employee_dict[person]
+            person_times_today = person_times[get_current_weekday()]
+            for person_timerange_today in person_times_today:
+                if person_timerange_today[0] < current_time and person_timerange_today[1] > current_time:
+                    if person_timerange_today[1] - current_time > 0.5:
+                        here_now_list.append(person)
+                        break
+                    else:
+                        leaving_soon_list.append(person)
+                        break
+                elif person_timerange_today[0] > current_time and person_timerange_today[0] - current_time < 0.5:
+                    arriving_soon_list.append(person)
+        self.here_now_frame.set_people_list(here_now_list)
+        self.leaving_soon_frame.set_people_list(leaving_soon_list)
+        self.arriving_soon_frame.set_people_list(arriving_soon_list)
 class PeopleFrame(tk.Frame):
     def __init__(self,root=None,title="",*args,**kwargs):
         tk.Frame.__init__(self,root,*args,**kwargs)
         self.title = tk.StringVar()
         self.title.set(title)
         tk.Label(self,textvariable=self.title,font=("Helvetica", bfs*3)).pack()
-        self.peopleLabels = []
-    def setPeopleList(self,peopleList): #edit for optimality later
-        for label in self.peopleLabels:
+        self.people_labels = []
+    def set_people_list(self,people_list): #edit for optimality later
+        for label in self.people_labels:
             label.pack_forget()
-        for person in peopleList:
+        for person in people_list:
             label = tk.Label(self,text=person,font=("Courier", bfs*2))
-            self.peopleLabels.append(label)
+            self.people_labels.append(label)
             label.pack()
 class DescriptionFrame(tk.Frame):
     def __init__(self,root=None,title="",*args,**kwargs):
         tk.Frame.__init__(self,root,*args,**kwargs)
         self.master = root
-        self.descLabel = tk.Label(self,text=self.master.locationDesc,font=("Helvetica", bfs*4))
-        self.descLabel.pack()
+        tk.Label(self,text=self.master.location_description,font=("Helvetica", bfs*4)).pack()
     
-class TimeFrame(tk.Frame):
+class time_frame(tk.Frame):
     def __init__(self,root=None,title="",*args,**kwargs):
         tk.Frame.__init__(self,root,*args,**kwargs)
         self.master = root
-        self.dayOfWeekVar = tk.StringVar()
-        self.timeVar = tk.StringVar()
-        self.dayOfWeekLabel = tk.Label(self,textvariable=self.dayOfWeekVar,font=("Helvetica", bfs*4))
-        self.timeLabel = tk.Label(self,textvariable=self.timeVar,font=("fixedsys",bfs*3) )
-        self.dayOfWeekLabel.pack()
-        self.timeLabel.pack()
-    def updateTimeFrame(self):
-        dayOfWeekPretty,timePretty = getPrettyTime()
-        self.dayOfWeekVar.set(dayOfWeekPretty)
-        self.timeVar.set(timePretty)
-def getTimeAsHalfInt():
+        self.day_of_week_label = tk.Label(self,text='hi',font=("Helvetica", bfs*4))
+        self.time_label = tk.Label(self,text="",font=("fixedsys",bfs*3) )
+        self.day_of_week_label.pack()
+        self.time_label.pack()
+    def update_time_frame(self):
+        self.day_of_week_label['text'], self.time_label['text'] = get_pretty_time()
+def get_current_time_as_float():
     now = datetime.datetime.now()
-    overHalf = (now.minute > 30)
-    hour = now.hour
-    if overHalf: hour+=0.5
-    return hour
-def getWeekdayAsChar():
-    intVersion = datetime.datetime.today().weekday()
-    weekdays = ['M','T','W','R','F','S','U']
-    return weekdays[intVersion]
-def loadPeopleFile(root):
+    return now.hour + now.minute/60
+def get_current_weekday():
+    int_version = datetime.datetime.today().weekday()
+    weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    return weekdays[int_version]
+def load_people_file(root):
     filename = filedialog.askopenfilename(parent=root,title="Please open the txt file of the employees.",filetypes=(("Text files","*.txt"),("All files","*.*")))
     with open(filename) as f:
         return eval(f.read())
-def getPrettyTime():
+def get_pretty_time():
     now = datetime.datetime.now()
     longTime = str(now.hour)+":"+str(now.minute)+":"+str(now.second)
-    dayOfWeekInt = datetime.datetime.today().weekday()
-    weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-    longWeekDay = weekdays[dayOfWeekInt]
-    return (longWeekDay,longTime)
-def strFromTime(time):
-    assert float(time*2).is_integer()
-    pm = (time >= 12)
-    fullHour = (float(time).is_integer())
-    hour = math.floor(time)%12
-    if (hour==0):
-        hour = 12
-    noonSwitchString = "PM" if pm else "AM"
-    halfHourString = "00" if fullHour else "30"
-    timeString = str(hour) + ":" + halfHourString + " " + noonSwitchString
-    return timeString
+    return (get_current_weekday(),longTime)
 def main():
     root=tk.Tk()
     thing = Application(root=root)
-    #print("3")
-    root.mainloop()
-    #print("4")
+    asyncio.run(thing.always_update())
 if  __name__ =='__main__':main()
